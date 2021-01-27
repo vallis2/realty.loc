b34f62a1c859942d581c345161fcca469b50a4f5
vallis2 <vc750870@gmail.com>
2021-01-27 14:02:36 +0200

    added header media

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+  // menu__btn
+  $('.menu__btn').on('click', function(){
+    $('.nav').slideToggle();
+  });
+
+  $('.bottom__menu-btn').on('click', function(){
+    $('.header__bottom-menu').slideToggle();
+  });
 
 
 });
